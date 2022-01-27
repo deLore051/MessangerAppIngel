1663935ab874d9c52433276fa5eb68e10138975d
deLore051 <stefan.dojcinovic051@gmail.com>
2022-01-27 00:09:41 -0800

    Initial Commit

---
+++ MessangerAppIngel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessangerAppIngel
+//
+//  Created by Ingel Agro on 27.1.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
